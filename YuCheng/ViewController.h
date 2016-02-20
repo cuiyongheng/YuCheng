cdba3f9becba12bb7e7fd50e5813274313a675be
apple <apple@amarimatoimac.local>
2016-02-20 16:02:03 +0800

    Initial Commit

---
+++ YuCheng/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YuCheng
+//
+//  Created by apple on 16/2/20.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
